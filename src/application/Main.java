44ddd19e2ea2b4f83f188b199c6c9ba50a806c38
MarcoAn778 <marco.duoli2@gmail.com>
2025-11-02 20:32:41 -0300

    About view and add Alerts and Constraints

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,29 +1,35 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
+
+	private static Scene scene;
+
 	@Override
 	public void start(Stage stage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			Scene scene = new Scene(scrollPane);
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
+			scene = new Scene(scrollPane);
 			stage.setScene(scene);
 			stage.setTitle("Workshop JavaFX");
 			stage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public static Scene getScene() {
+		return scene;
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
